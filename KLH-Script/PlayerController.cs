f6455a85d4349f6697370e90c2262f7a0b8ac9fc
Prashantkhatri99 <119466896+Prashantkhatri99@users.noreply.github.com>
2025-02-14 17:05:48 +0545

    Add files via upload

--- KLH-Script/PlayerController.cs
+++ KLH-Script/PlayerController.cs
@@ -1,85 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    [Header("Horizontal Movement Settings")]
     private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 moveInput;
+    
+    public float walkSpeed = 5f;
+    public float runSpeed = 8f; // Running speed
+    
+    public bool IsMoving { get; private set; }
+    public bool IsRunning { get; private set; }
 
-    [SerializeField]
-    private float walkSpeed = 5f; // Movement speed
-
-    private float xAxis;
-
-    [Header("Ground Check Settings")]
-    [SerializeField] private float jumpForce = 15f;
-
-    [SerializeField] private Transform groundCheckPoint;
-
-    [SerializeField] private float groundCheckY = 0.7f;
-
-    [SerializeField] private float groundCheckX = 0.5f;
-
-    [SerializeField] private LayerMask whatIsGround;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        GetInput();
-        HandleJump();
+        float speed = IsRunning ? runSpeed : walkSpeed;
+        rb.velocity = new Vector2(moveInput.x * speed * Time.fixedDeltaTime, rb.velocity.y);
     }
 
-    void FixedUpdate()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        MovePlayer();
+        moveInput = context.ReadValue<Vector2>();
+        IsMoving = moveInput != Vector2.zero;
+        animator.SetBool("isMoving", IsMoving);
     }
 
-    void GetInput()
+    public void OnRun(InputAction.CallbackContext context)
     {
-        xAxis = Input.GetAxisRaw("Horizontal"); // Get horizontal input
-    }
+        if (context.started)
+            IsRunning = true;
+        else if (context.canceled)
+            IsRunning = false;
 
-    void MovePlayer()
-    {
-        // Apply movement based on input and speed
-        rb.velocity = new Vector2(xAxis * walkSpeed, rb.velocity.y);
-    }
-
-   public bool Grounded()
-{
-    bool isGrounded = Physics2D.Raycast(groundCheckPoint.position, Vector2.down, groundCheckY, whatIsGround) ||
-                      Physics2D.Raycast(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround) ||
-                      Physics2D.Raycast(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down, groundCheckY, whatIsGround);
-
-    Debug.DrawRay(groundCheckPoint.position, Vector2.down * groundCheckY, Color.red);
-    Debug.DrawRay(groundCheckPoint.position + new Vector3(groundCheckX, 0, 0), Vector2.down * groundCheckY, Color.red);
-    Debug.DrawRay(groundCheckPoint.position + new Vector3(-groundCheckX, 0, 0), Vector2.down * groundCheckY, Color.red);
-
-    Debug.Log($"Grounded: {isGrounded}");
-    return isGrounded;
-}
-
-
-    void HandleJump()
-    {
-        // Stop upward velocity when jump button is released
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-        }
-
-        // Allow jump only if the player is grounded and the Jump button is pressed
-        if (Input.GetButtonDown("Jump") && Grounded())
-        {
-            Debug.Log("Jump button pressed");
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+        animator.SetBool("isRunning", IsRunning);
     }
 }
