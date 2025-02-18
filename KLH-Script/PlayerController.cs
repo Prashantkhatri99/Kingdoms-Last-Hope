d3f9416b42ed9c4d8917c3f56214e3fe4db891b1
Prashantkhatri99 <119466896+Prashantkhatri99@users.noreply.github.com>
2025-02-18 20:18:06 +0545

    Add files via upload

--- KLH-Script/PlayerController.cs
+++ KLH-Script/PlayerController.cs
@@ -6,42 +6,125 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    public float walkSpeed = 5f; // Normal walking speed
+    public float runSpeed = 8f;  // Running speed
+    private Vector2 moveInput;   // Stores player movement input
+
+    // Property to determine current movement speed
+    public float currentMoveSpeed 
+    { 
+        get 
+        {
+            if (IsMoving) // If the player is moving
+            {
+                if (IsRunning) // If the player is running
+                {
+                    return runSpeed;
+                } 
+                else // If the player is walking
+                {
+                    return walkSpeed;
+                }
+            }
+            else 
+            {
+                return 0; // Player is idle
+            }
+        } 
+    }
+
+    [SerializeField]
+    private bool _isMoving = false;
+
+    public bool IsMoving
+    {
+        get { return _isMoving; }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value); // Update animator parameter
+        }
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+
+    public bool IsRunning
+    {
+        get { return _isRunning; }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value); // Update animator parameter
+        }
+    }
+
+    // Player direction variables
+    public bool _isFacingRight = true;
+
+    public bool IsFacingRight 
+    { 
+        get { return _isFacingRight; } 
+        private set
+        {
+            if (_isFacingRight != value) // Check if the direction has changed
+            {
+                _isFacingRight = value; // Update the facing direction
+                
+                // 🔧 FIX: Ensure scale doesn't go to zero or cause rendering issues
+                Vector3 newScale = transform.localScale;
+                newScale.x = Mathf.Abs(newScale.x) * (_isFacingRight ? 1 : -1);
+                transform.localScale = newScale;
+            }
+        } 
+    }
+
     private Rigidbody2D rb;
     private Animator animator;
-    private Vector2 moveInput;
-    
-    public float walkSpeed = 5f;
-    public float runSpeed = 8f; // Running speed
-    
-    public bool IsMoving { get; private set; }
-    public bool IsRunning { get; private set; }
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();  // Get Rigidbody2D component
+        animator = GetComponent<Animator>(); // Get Animator component
     }
 
-    private void FixedUpdate()
-    {
-        float speed = IsRunning ? runSpeed : walkSpeed;
-        rb.velocity = new Vector2(moveInput.x * speed * Time.fixedDeltaTime, rb.velocity.y);
-    }
+   private void FixedUpdate()
+{
+    // Move the player using the calculated movement speed
+    rb.velocity = new Vector2(moveInput.x * currentMoveSpeed, rb.velocity.y);
+    animator.SetFloat(AnimationStrings.yVelocity, rb.velocity.y);
+}
 
     public void OnMove(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
-        IsMoving = moveInput != Vector2.zero;
-        animator.SetBool("isMoving", IsMoving);
+        moveInput = context.ReadValue<Vector2>(); // Read movement input
+        IsMoving = moveInput != Vector2.zero; // Check if the player is moving
+        SetFacingDirection(moveInput); // Update the facing direction
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight)
+        {
+            // Face right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight)
+        {
+            // Face left
+            IsFacingRight = false;
+        }
     }
 
     public void OnRun(InputAction.CallbackContext context)
     {
         if (context.started)
-            IsRunning = true;
+        {
+            IsRunning = true; // Enable running
+        }
         else if (context.canceled)
-            IsRunning = false;
-
-        animator.SetBool("isRunning", IsRunning);
+        {
+            IsRunning = false; // Disable running
+        }
     }
 }
