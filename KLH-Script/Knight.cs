dfdd13ba406e49691952a3ab976a35a900599105
Prashantkhatri99 <119466896+Prashantkhatri99@users.noreply.github.com>
2025-03-01 23:21:36 +0545

    Made knight to detect player and attack

--- KLH-Script/Knight.cs
+++ KLH-Script/Knight.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float walkSpeed = 3f;
+    public DetectionZone attackZone;
     Rigidbody2D rb;
     TouchingDirections touchingDirections;
+    Animator animator;
     public enum WalkableDirection { Right, Left }
     private WalkableDirection _walkDirection;
     private Vector2 walkDirectionVector = Vector2.right;
@@ -36,12 +38,30 @@ public class Enemy : MonoBehaviour
           // Assign new direction
             _walkDirection = value; }
     }
+    public bool _hasTarget = false;
+
+    public bool HasTarget 
+    { 
+        get{return _hasTarget;} 
+        private set
+        {
+            _hasTarget = value;
+            animator. SetBool(AnimationStrings. hasTarget, value);
+        }
+    }
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         touchingDirections = GetComponent<TouchingDirections>();
+        animator = GetComponent<Animator>();
+    }
+
+        // Update is called once per frame
+    void Update()
+    {
+        HasTarget = attackZone.detectedColliders.Count > 0;
     }
 
     private void FixedUpdate()
@@ -76,9 +96,5 @@ public class Enemy : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
