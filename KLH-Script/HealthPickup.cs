24b5e34ad1656317a592e3ef8dd777cea5d32760
Prashantkhatri99 <119466896+Prashantkhatri99@users.noreply.github.com>
2025-05-04 16:21:50 +0545

    Health pickup

--- KLH-Script/HealthPickup.cs
+++ KLH-Script/HealthPickup.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class HealthPickup : MonoBehaviour
 {
+    public int healthRestore = 20;
+    public Vector3 spinRotationSpeed = new Vector3(0, 180, 0);
+
     // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        Damageable damageable = collision.GetComponent<Damageable>();
+        if (damageable)
+        {
+            bool wasHealed = damageable.Heal(healthRestore);
+            if (wasHealed)
+                Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.eulerAngles += spinRotationSpeed * Time.deltaTime;
     }
 }
